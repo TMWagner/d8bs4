fc8a1667264ec58ed387d2f9834fa58d20f60ed8
Thomas Wagner <tmw100@gmail.com>
2018-10-18 14:52:04 -0700

    drupal 8.6.2
    Fresh install

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit1fc4236cd496c57dc992db6a839ab10f::getLoader();
+return ComposerAutoloaderInit28311d13f5f79f508faf4b2d0a4ab7ba::getLoader();
