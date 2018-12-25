bbac944718fb1c537496a820ff18769a9432a23c
Thomas Wagner <tmw100@gmail.com>
2018-12-24 16:36:15 -0800

    initial theme commit

---
+++ web/modules/custom/idm/js/idm.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * Coding Standards: https://www.drupal.org/node/172169
+ */
+
+(() => {
+
+  //$(this).next().slideToggle('slow');
+  //$('a.gallery').colorbox();
+  //Debug code
+  (function ($) {
+  //your code, now you can use "$" with no problems
+  $('#link').css("background-color", "yellow");
+  })(jQuery);
+  
+
+  // All the JavaScript for this file.
+
+})();
---
+++ web/modules/custom/idm/js/mega.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+(function ($, Drupal) {
+
+  'use strict';
+  /* CODE GOES HERE  - Code Wrap*/
+ $(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+
+  /* END Code Wrap */
+})(jQuery, Drupal);
