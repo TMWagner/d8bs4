b3ebbfd4c2b524242b6cf450e64505c8abc9c4e6
Thomas Wagner <tmw100@gmail.com>
2019-02-04 11:07:07 -0800

    Incremental working

--- web/modules/custom/people/src/Controller/PeopleController.php
+++ web/modules/custom/people/src/Controller/PeopleController.php
@@ -31,47 +31,50 @@ class PeopleController extends ControllerBase {
    */
   public function content() {
 
+    $uid = '133';
+    $profile_id = 'profile_basic_content';
+    $display_id = 'user_profile_desktop';
+
+
+    $view_output = views_embed_view($profile_id, $display_id, $uid);
+
+    $page['profile']['bio_mobile'] = array(
+      '#type' => 'block',
+      'content' => [
+        'system_main' => $view_output,
+      ],
+      '#prefix' => '<div class="mobile-display">',
+      '#suffix' => '</div>',
+    );
+
+
     $render_array['page'] = [
       '#type' => 'markup',
       '#markup' => 'Profile page',
       '#prefix' => '<div><h1>',
       '#suffix' => '</h1></div>',
     ];
-    return $render_array;
+    return $page;
   }
 
   public function arguments($arg1) {
-    // Load profile
-    $userProfile = \Drupal\user\Entity\User::load($arg1);
+//    // Load profile
+//    $userProfile = \Drupal\user\Entity\User::load($arg1);
 
-    // Get field data from that user.
-    $firstname = $userProfile->get('field_user_first_name')->value;
-    $lastname = $userProfile->get('field_user_last_name')->value;
-    $title = $userProfile->get('field_user_title')->value;
-    $userpicture = $userProfile->get('user_picture')->value;
-    $videolink = $userProfile->get('field_user_video_link')->value;
+    // Set view arguments
+    $profile_id = 'profile_basic_content';
+    $display_id = 'user_profile_desktop';
 
 
-    // Staff snippet.
-
-    $view = \Drupal\views\Views::getView('profile_basic_content');
+    // Load view object
+    $view = \Drupal\views\Views::getView($profile_id);
     $view->setDisplay('user_profile_desktop');
-//    $view->preExecute();
     $view->setArguments(array($arg1));
     $view->execute();
+    $variables['content'] = $view->buildRenderable($display_id);
 
-    $variables['content'] = $view->buildRenderable('user_profile_desktop');
 
 
-    $list[] = $this->t("First number was @number.", ['@number' => $arg1]);
-
-    $render_array['page_example_arguments'] = [
-      // The theme function to apply to the #items.
-      '#theme' => 'item_list',
-      // The list itself.
-      '#items' => $list,
-      '#title' => $this->t('Argument Information'),
-    ];
 
 
     $render_array['page_test'] = [
@@ -79,12 +82,12 @@ class PeopleController extends ControllerBase {
       '#content' => $variables,
     ];
 
-    $render_array['page_example_arguments_firstname'] = [
-      '#type' => 'markup',
-      '#markup' => $firstname . ' ' . $lastname,
-      '#prefix' => '<div><h1>',
-      '#suffix' => '</h1></div>',
-    ];
+//    $render_array['page_example_arguments_firstname'] = [
+//      '#type' => 'markup',
+//      '#markup' => $firstname . ' ' . $lastname,
+//      '#prefix' => '<div><h1>',
+//      '#suffix' => '</h1></div>',
+//    ];
 
     //Got this with drupal debug:router | grep 'publications'
     $url = Url::fromRoute('view.publications_listings.publications_listing');
