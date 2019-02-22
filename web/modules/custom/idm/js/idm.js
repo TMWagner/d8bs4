30c222831c156d71b616708944642290029015fb
Thomas Wagner <tmw100@gmail.com>
2019-02-22 11:08:29 -0800

    cleanup

--- web/modules/custom/idm/js/idm.js
+++ web/modules/custom/idm/js/idm.js
@@ -25,16 +25,28 @@
   // });
 
 
-  $('#modellink').click(function() {
-
+  //Begin modal trigger
+  //
+  $('.modal-link').click(function() {
     //  URL passed from the click universal object
     var url = $(this).attr('href');
 
     // @todo See http://api.jquery.com/load/: We can specify what part of the doc we want...
+    console.log("URL is: " + url);
+    /*
+    * .modal-container: location on parent page where model will load
+    * .#myModal: stuff we are going to load (not sure what is the difference
+    *   Between that and what we can put in the parameter.
+    *   @todo See http://api.jquery.com/load/: We can specify what part of the doc we want...
+     */
     $('.modal-container').load(url,function(result){
       $('#myModal').modal({show:true});
+
     });
   });
+  // End Modal Trigger
+
+
 
   /* END Code Wrap */
 })(jQuery, Drupal);
