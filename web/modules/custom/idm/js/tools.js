b5f2940bfa22d2552c2f913ad142809ad41053e9
Thomas Wagner <tmw100@gmail.com>
2019-06-16 08:44:23 -0700

    Change class for insert to be more meaningful

--- web/modules/custom/idm/js/tools.js
+++ web/modules/custom/idm/js/tools.js
@@ -394,8 +394,7 @@
     $("#ModalTitle").text(cardClicked.find('h4').html());
 
     // Grab just the body text
-    $( ".insert" ).load(data + " .tools-card-text");
-
+    $( ".insertbody" ).load(data + " .tools-card-text");
 
     // Add buttons
     $(".insertbuttons").load(data + " #tool-sublink-buttons");
